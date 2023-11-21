15afae233e56531b4a6356d80b9aae1208f21700
王超毅 <wangchaoyi@wangchaoyideMacBook-Pro.local>
2023-11-21 19:23:24 +0800

    cannot produce right pictures

--- hdrnet/tem_tf2.py
+++ hdrnet/tem_tf2.py
@@ -12,6 +12,9 @@ import skimage
 import skimage.io
 import skimage.transform
 
+import bilateral_slice_tf2 as bs
+import bilateral_slice as bsj
+
 def get_value_from_checkpoint(save_path):
 
   reader = tf.train.load_checkpoint(save_path)
@@ -82,6 +85,75 @@ def fusion(grid_features,global_features,batch_size):
   fusion = tf.nn.relu(fusion_grid+fusion_global)
   return fusion
 
+def predic_model():
+
+  model = tf.keras.Sequential() 
+  model.add(layers.Conv2D(96,(1,1),strides=(1,1), padding='same', activation=None, input_shape=(16,16,64)))
+  return model
+
+def packed_coefficients(current_layer):
+  numImgChs=3
+  n_out=3
+  n_in=4
+  current_layer = tf.stack(
+    tf.split(current_layer, n_out*n_in, axis=3), axis=4)
+  current_layer = tf.stack(
+    tf.split(current_layer, n_in, axis=4), axis=5)
+  return current_layer
+
+def guide_color(ful_res,ccm,ccm_bias):
+  npts=16
+  # nchans = ful_res.get_shape().as_list()[-1]
+  nchans = ful_res.shape[-1]
+  # ccm = tensors['vars/inference.Sguide.Sccm/.ATTRIBUTES/VARIABLE_VALUE']
+
+  # ccmbias=tensors['vars/inference.Sguide.Sccm_bias/.ATTRIBUTES/VARIABLE_VALUE']
+  guidemap = tf.matmul(tf.reshape(ful_res, [-1, nchans]), ccm)
+  guidemap = tf.nn.bias_add(guidemap, ccm_bias, name='ccm_bias_add')
+  guidemap = tf.reshape(guidemap, tf.shape(input=ful_res))
+
+  return guidemap
+  
+def guide_conv():
+  model=tf.keras.Sequential()
+  model.add(layers.Conv2D(1, (1, 1), strides=(1, 1), padding='same', activation=None, input_shape=(2048,2048,3)))
+  return model
+
+
+def guide_curve(guidemap,shifts,slopes,guide_conv_w,guide_conv_b):
+  # shifts_ = np.linspace(0, 1, npts, endpoint=False, dtype=np.float32)
+  # shifts_ = shifts_[np.newaxis, np.newaxis, np.newaxis, :]
+  # shifts_ = np.tile(shifts_, (1, 1, nchans, 1))
+
+  guidemap = tf.expand_dims(guidemap, 4)
+  # shifts = tfv1.get_variable('shifts', dtype=tf.float32, initializer=shifts_)
+  # shifts = tensors['vars/inference.Sguide.Sshifts/.ATTRIBUTES/VARIABLE_VALUE']
+
+  # slopes_ = np.zeros([1, 1, 1, nchans, npts], dtype=np.float32)
+  # slopes_[:, :, :, :, 0] = 1.0
+  # slopes = tfv1.get_variable('slopes', dtype=tf.float32, initializer=slopes_)
+  # slopes = tensors['vars/inference.Sguide.Sslopes/.ATTRIBUTES/VARIABLE_VALUE']
+  guidemap = tf.reduce_sum(input_tensor=slopes*tf.nn.relu(guidemap-shifts), axis=[4])
+
+  # guidemap = tf.keras.layers.Conv2D(
+  #     filters=1, 
+  #     kernel_size=1, 
+  #     kernel_initializer=tfv1.constant_initializer(1.0/nchans),
+  #     bias_initializer=tfv1.constant_initializer(0),
+  #     activation=None,
+  #     name='channel_mixing'
+  #     )(guidemap)
+
+  model = guide_conv()
+  model.layers[0].set_weights([guide_conv_w,guide_conv_b])
+
+  guidemap = model(guidemap)
+  guidemap = tf.clip_by_value(guidemap, 0, 1)
+  guidemap = tf.squeeze(guidemap, axis=[3,])
+  return guidemap
+# __________________________
+
+
 
 model1=splat()
 weight1 = tensors['vars/inference.Scoefficients.Ssplat.Sconv1.Sweights/.ATTRIBUTES/VARIABLE_VALUE']
@@ -134,6 +206,25 @@ weightglobal5 = tensors['vars/inference.Scoefficients.Sglobal.Sfc3.Sweights/.ATT
 biasglobal5 = tensors['vars/inference.Scoefficients.Sglobal.Sfc3.Sbiases/.ATTRIBUTES/VARIABLE_VALUE']
 model3.layers[9].set_weights([weightglobal5, biasglobal5])
 
+model4=predic_model()
+
+weightpredic1=tensors['vars/inference.Scoefficients.Sprediction.Sconv1.Sweights/.ATTRIBUTES/VARIABLE_VALUE']
+biaspredic1=tensors['vars/inference.Scoefficients.Sprediction.Sconv1.Sbiases/.ATTRIBUTES/VARIABLE_VALUE']
+model4.layers[0].set_weights([weightpredic1,biaspredic1])
+
+ccm = tensors['vars/inference.Sguide.Sccm/.ATTRIBUTES/VARIABLE_VALUE']
+ccm_bias=tensors['vars/inference.Sguide.Sccm_bias/.ATTRIBUTES/VARIABLE_VALUE']
+
+shifts = tensors['vars/inference.Sguide.Sshifts/.ATTRIBUTES/VARIABLE_VALUE']
+slopes = tensors['vars/inference.Sguide.Sslopes/.ATTRIBUTES/VARIABLE_VALUE']
+guide_conv_w = tensors['vars/inference.Sguide.Schannel_mixing.Sweights/.ATTRIBUTES/VARIABLE_VALUE']
+guide_conv_b = tensors['vars/inference.Sguide.Schannel_mixing.Sbiases/.ATTRIBUTES/VARIABLE_VALUE']
+
+# ____________________
+
+
+
+# ____________________
 
 im_input=cv2.imread("../sample_data/input.png",-1)
 print(im_input.shape)
@@ -144,11 +235,13 @@ lowres_input = np.expand_dims(lowres_input, axis=0)
 lowres_input = skimage.img_as_float(lowres_input)
 # lowres_input = lowres_input/255.0
 # lowres_input = np.clip(lowres_input,0,1)
+fulres_input = np.expand_dims(im_input, axis=0)
+fulres_input=skimage.img_as_float(fulres_input)
 
 print(lowres_input)
 print(lowres_input.shape)
 
-splat_features=model1.predict(lowres_input)
+splat_features=model1(lowres_input)
 print(splat_features*255.0)
 print(splat_features.shape)
 
@@ -162,5 +255,55 @@ print(global_features)
 
 fused_features=fusion(local_features,global_features,1)
 print(fused_features.shape)
-print(fused_features)
-print(tf.math.count_nonzero(fused_features))
+# print(fused_features)
+# print(tf.math.count_nonzero(fused_features))
+
+predic_features=model4(fused_features)
+print(predic_features)
+print(predic_features.shape)
+print("if less zero")
+has_negative = has_negative = tf.reduce_any(predic_features < 0)
+print(has_negative.numpy())
+
+packed_features=packed_coefficients(predic_features)
+print("packed_features")
+print(packed_features.shape)
+print("if less zero")
+has_negative = has_negative = tf.reduce_any(packed_features < 0)
+print(has_negative.numpy())
+
+guidemap = guide_color(fulres_input,ccm,ccm_bias)
+
+guidemap = guide_curve(guidemap,shifts,slopes,guide_conv_w,guide_conv_b)
+# print("guidemap:")
+# print(guidemap)
+
+gray_image_array = np.array(guidemap)
+packed_features_np = np.array(packed_features)
+
+sliced = bsj.bilateral_slice(packed_features_np[0],gray_image_array[0])
+# tem = bs.local_bilateral_slice(guidemap,packed_features)
+print("sliced:")
+print(sliced.shape)
+
+fulres_input_tf=tf.convert_to_tensor(fulres_input,dtype=tf.float32)
+sliced_tf=tf.expand_dims(tf.convert_to_tensor(sliced,dtype=tf.float32),axis=0)
+# print(sliced_tf.get_shape().as_list())
+output = bs.apply(sliced_tf,fulres_input_tf)
+print(output)
+
+
+image_array = output[0].numpy()*255.0
+image_array = image_array.astype(np.uint8)
+print(image_array)
+# skimage.io.imsave('image.png', image_array)
+# # 从数组中提取灰度图像
+# gray_image_array = np.squeeze(gray_image_array, axis=0)
+# gray_image_array = gray_image_array * 255.0
+
+# # 将像素值转换为 8 位无符号整数
+# gray_image_array = gray_image_array.astype(np.uint8)
+# print(gray_image_array)
+
+# # 保存图像
+# skimage.io.imsave('gray_image.jpg', gray_image_array)
